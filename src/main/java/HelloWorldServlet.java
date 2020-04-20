41831d076daae1a7afef21e60fec992bc6cc8285
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-20 16:53:22 -0500

    completed bonus 1

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,7 +1,10 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.applet.Applet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -10,11 +13,33 @@ import java.util.Scanner;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
-        Scanner s = new Scanner(System.in);
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException, ServletException {
+
+
+        String name = req.getParameter("name");
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
-        System.out.println();
+
+        if (name != null){
+            try {
+                out.println("<h1>Hello " + name + "!</h1>");
+            } catch (Exception e) {
+                response.resetBuffer();
+                out.println("<h1>Hello, World!</h1>");
+            }
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
+
+//        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/banner");
+//        if (dispatcher != null){
+//            dispatcher.include(req, response);
+//        }
+
     }
+//    @Override
+//    public void destroy(){
+//        getServletContext().log("destroy() called");
+//    }
 }
