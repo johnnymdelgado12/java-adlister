0de00d8ec32c8e60e5cf6e1f27499d5d41e75054
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-28 11:13:48 -0500

    added logout servlet

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
+
+        // inside of a servlet
+        HttpSession session = request.getSession();
+
+
+        if (session.getAttribute("user") != null){
+            request.setAttribute("user",session.getAttribute("user"));
+            request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request,response);
+        }else {
+            response.sendRedirect("/login");
+        }
+
+
     }
 }
---
+++ src/main/webapp/WEB-INF/secret.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: johnnydelgado
+  Date: 4/23/20
+  Time: 3:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>secret</title>
+</head>
+<body>
+
+</body>
+</html>
